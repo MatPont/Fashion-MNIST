7c42240ad65b354afca0fd4b9660c48f4b06b590
Mathieu Pont <matthieu.pont@hotmail.fr>
2019-04-18 12:02:29 +0200

    compute tSNE time

--- t_SNE.R
+++ t_SNE.R
@@ -38,25 +38,45 @@ test_xy = cbind(test_x, test_y)
 
 label_col = 785
 
-perpl <- 5
-for(i in c(1:10)){
+times <- c()
+
+#perpl <- 5
+
+perpl_v <- c(1:20 * 5, 1:10 * 25 + 100)
+
+save_plot = FALSE
+
+#for(i in c(1:10)){
+for(perpl in perpl_v){
   # Run tSNE
+  start_time <- Sys.time()
   tsne <- Rtsne(train_x, perplexity = perpl, eta = 200.0, verbose = TRUE) 
+  stop_time <- Sys.time()
+  time_taken <- stop_time - start_time
+  times <- c(times, time_taken)
   
   # Plot
-  name <- paste("tsne_", perpl, ".png", sep = "")
-  colors = rainbow(length(unique(train_y)))
-  names(colors) = unique(train_y)
-  kl <- round(tsne$itercosts[length(tsne$itercosts)], digits = 2)
-  plot_name <- paste("t-SNE with KL-divergence = ", kl, sep ="")
-  png(filename=name, width = 1000, height = 1000)
-  plot(tsne$Y, xlab = "Comp. 1", ylab = "Comp. 2", col = colors[train_y], main = plot_name)
-  dev.off()
+  if(save_plot){
+    name <- paste("tsne_", perpl, ".png", sep = "")
+    colors = rainbow(length(unique(train_y)))
+    names(colors) = unique(train_y)
+    kl <- round(tsne$itercosts[length(tsne$itercosts)], digits = 2)
+    plot_name <- paste("t-SNE with KL-divergence = ", kl, sep ="")
+    png(filename=name, width = 1000, height = 1000)
+    plot(tsne$Y, xlab = "Comp. 1", ylab = "Comp. 2", col = colors[train_y], main = plot_name)
+    dev.off() 
+  }
   
   # Increase perplexity
-  perpl <- perpl +5
+  #perpl <- perpl +5
 }
 
-kl_res <- c(4.09, 3.73, 3.48, 3.33, 3.18, 3.08, 2.99, 2.91, 2.83, 2.78, 2.73, 2.68, 2.62, 2.56, 2.54, 2.51, 2.47, 2.43, 2.39, 2.36, 2.21, 2.14, 2.03, 1.97, 1.88, 1.83, 1.77, 1.72, 1.67, 1.64)
-perpl_v <- c(1:20 * 5, 1:10 * 25 + 100)
-plot(perpl_v, kl_res, type = "b", xlab = "perplexity", ylab = "KL divergence")
+print(times)
+
+png(filename="tSNE_time", width = 1000, height = 1000)
+plot(perpl_v, times, type = "b", xlab = "Perplexity", ylab = "Computation time")
+dev.off() 
+
+#kl_res <- c(4.09, 3.73, 3.48, 3.33, 3.18, 3.08, 2.99, 2.91, 2.83, 2.78, 2.73, 2.68, 2.62, 2.56, 2.54, 2.51, 2.47, 2.43, 2.39, 2.36, 2.21, 2.14, 2.03, 1.97, 1.88, 1.83, 1.77, 1.72, 1.67, 1.64)
+#perpl_v <- c(1:20 * 5, 1:10 * 25 + 100)
+#plot(perpl_v, kl_res, type = "b", xlab = "perplexity", ylab = "KL divergence")
